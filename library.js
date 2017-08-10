8a551f97f2727ab69d720ca22a1ad65f0c5f3014
DESKTOP-U6KV48T\Techtonic Academy <austinmoses@ymail.com>
2017-08-10 13:06:51 -0600

    Full Working Library

--- library.js
+++ library.js
@@ -1,18 +1,16 @@
-var Library = function() {};
+var Library = function() {
+  this.myBookArr = new Array();
+};
 
 var Book = function(oArgs) {
   this.title = oArgs.title;
   this.author = oArgs.author;
   this.numPages = oArgs.numPages;
-  this.aDate = oArgs.date; //.split("/");
+  this.pDate = new Date(oArgs.date);
   // this.pubDate = new Date(this.aDate[0], this.aDate[1]);
 };
 
-// var Books = Library.prototype.myBookArr
-
-Library.prototype.myBookArr = [];
-
-//Add Book
+/////////////////////////////////////////////////////////////Add Book/////////////////////////////////////////////////////////////////
 Library.prototype.addBook = function(book) {
   for(var i = 0; i < this.myBookArr.length; i++) {
     if(this.myBookArr[i].title == book.title) {
@@ -24,8 +22,7 @@ Library.prototype.addBook = function(book) {
   return true;
 };
 
-//removeBookByTitle
-
+//////////////////////////////////////////////////////////////removeBookByTitle////////////////////////////////////////////////////////////////
 Library.prototype.removeBookByTitle = function(title) {
 var bool = false;
 for(var i = 0; i < this.myBookArr.length; i++) {
@@ -37,8 +34,7 @@ for(var i = 0; i < this.myBookArr.length; i++) {
   return bool;
 };
 
-//removeBookByAuthor
-
+//////////////////////////////////////////////////////////////////removeBookByAuthor/////////////////////////////////////////////////////////////
 Library.prototype.removeBookByAuthor = function(author) {
 var bool = false;
 for(var i = 0; i < this.myBookArr.length; i++) {
@@ -50,7 +46,7 @@ for(var i = 0; i < this.myBookArr.length; i++) {
   return bool;
 };
 
-//getRandomBook
+/////////////////////////////////////////////////////////////////getRandomBook///////////////////////////////////////////////////////////////////
 Library.prototype.getRandomBook = function() {
   var randomBook = Math.floor(Math.random()*this.myBookArr.length);
   return this.myBookArr.length <= 0 ? null : this.myBookArr[randomBook];
@@ -60,7 +56,7 @@ Library.prototype.getRandomBook = function() {
   // return null;
 };
 
-//getBookByTitle
+/////////////////////////////////////////////////////////////getBookByTitle/////////////////////////////////////////////////////////////////////
 Library.prototype.getBookByTitle = function(title) {
   var titleArr = new Array();
   var reg = new RegExp(title, "gi");
@@ -72,7 +68,7 @@ Library.prototype.getBookByTitle = function(title) {
     return titleArr;
 };
 
-//getBooksByAuthor
+/////////////////////////////////////////////////////////getBooksByAuthor////////////////////////////////////////////////////////////////////////
 Library.prototype.getBooksByAuthor = function(author) {
   var byAuthorArr = new Array();
   var reg = new RegExp(author, "gi");
@@ -84,8 +80,7 @@ Library.prototype.getBooksByAuthor = function(author) {
   return byAuthorArr;
 };
 
-//addBooks
-
+////////////////////////////////////////////////////////////addBooks/////////////////////////////////////////////////////////////////////
 Library.prototype.addBooks = function(addBooksArr) {
   var numberBooksAdded = 0;
   for(i = 0; i < addBooksArr.length; i++) {         //we use this for loop to loop through the iterations of the loop we're passing through. In other words, the loop we're iterating through does not exist until we pass it into our function.
@@ -96,12 +91,11 @@ Library.prototype.addBooks = function(addBooksArr) {
     return numberBooksAdded;
   };
 
-  //getAuthors
-
+  //////////////////////////////////////////////////////////////getAuthors/////////////////////////////////////////////////////////////////////
   Library.prototype.getAuthors = function() {
     var authorListArr = new Array();
-    loop: for(i = 0; i < this.myBookArr.length; i++) {
-      for(a = 0; a < authorListArr.length; a++) {
+    for(i = 0; i < this.myBookArr.length; i++) {
+      loop: for(a = 0; a < authorListArr.length; a++) {
       if(this.myBookArr[i].author == authorListArr[a]) {
         break loop;
         }
@@ -111,8 +105,7 @@ Library.prototype.addBooks = function(addBooksArr) {
     return authorListArr;
   };
 
-  //getRandomAuthorName
-
+  //////////////////////////////////////////////////////////getRandomAuthorName///////////////////////////////////////////////////////////////
   Library.prototype.getRandomAuthorName = function() {
     var randomAuthor = Math.floor(Math.random()*this.myBookArr.length);
     return this.myBookArr.length <= 0 ? null : this.myBookArr[randomAuthor].author;
@@ -121,7 +114,8 @@ Library.prototype.addBooks = function(addBooksArr) {
 var denLib = new Library();
 var boulLib = new Library();
 
-var BookOne = new Book({title: "IT", author: "Stephen King", numPages: 390, date: "03/12/1987"});
+var BookOne = new Book({title: "IT", author: "Stephen King", numPages: 390, date: "05/05/1999"});
 var BookTwo = new Book({title: "Harry Potter", author: "JK Rowling", numPages: 400, date: "05/05/2000"});
 var BookThree = new Book({title: "Fuck you Javascript", author: "Austin Moses", numPages: 1000000, date: "08/08/2017"});
 var BookFour = new Book({title: "Harry Potter 2", author: "JK Rowling", numPages: 450, date: "05/05/2001"});
+var BookFive = new Book({title: "The Great Gatsby", author: "Some Asshole", numPages: 215, date: "06/30/1970"});
